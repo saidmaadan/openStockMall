c480bf22520048ecd2103eb16f38a44a5e5d9120
Said Maadan <saidmaadan@users.noreply.github.com>
2016-04-19 00:09:12 -0500

    Configured State and added placeholder for stock view template

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('openStock.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
@@ -41,16 +41,26 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+.controller('MyStocksCtrl', ["$scope",
+function($scope) {
+  $scope.myStocksArray = [
+    {ticker: "APPL"},
+    {ticker: "GPRO"},
+    {ticker: "FB"},
+    {ticker: "NFLX"},
+    {ticker: "TSLA"},
+    {ticker: "BRK-A"},
+    {ticker: "INTC"},
+    {ticker: "MSFT"},
+    {ticker: "GE"},
+    {ticker: "BAC"},
+    {ticker: "C"},
+    {ticker: "T"}
   ];
-})
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+}])
+
+.controller('StockCtrl', ['$scope', '$stateParams',
+ function($scope, $stateParams) {
+   $scope.ticker = $stateParams.stockTicker;
+}]);
